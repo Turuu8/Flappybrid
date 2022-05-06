a0ac9366f28846c95add22bffe6499b457f87310
Turuu30 <103709712+Turuu30@users.noreply.github.com>
2022-05-06 13:00:34 +0800

    5.6

--- flappybrid.js
+++ flappybrid.js
@@ -1,6 +1,7 @@
 console.log("working");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const start = document.getElementsByTagName("button")[0];
 
 function Brid(x , y , r , color){
     this.x = x;
@@ -15,16 +16,55 @@ function Brid(x , y , r , color){
         ctx.fill();
     }
 
-    this.move =  function(){
-        ctx
+    this.move =  function(x , y){
+        this.x = this.x+x;
+        this.y = this.y+y;
+        ctx.clearRect(0,0, 700 , 800);
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x , this.y , this.r , 0,2*Math.PI);
+        ctx.fill();
+    }
+
+    this.down = (x , y) =>{
+        this.x = this.x-x;
+        this.y = this.y+y;
+        ctx.clearRect(0,0, 700 , 800);
+        ctx.beginPath(); 
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x , this.y , this.r , 0,2*Math.PI);
+        ctx.fill();
     }
 }
-
-const brid = new Brid(70 , 400 , 30 , "pink");
+function Way() {
+    t
+}
+const brid = new Brid(70 , 350 , 30 , "pink");
 brid.draw();
 
 
+window.addEventListener("click" , (ev) =>{
+    start.style.display ="none"
+    canvas.style.opacity  = "1"
+        setInterval(() =>{
+            brid.down(0 , 10)
+        },40) 
+        if(ev.keyCode == 80){
+            brid.down(0 , 0)
+        }
+        window.addEventListener('keydown' , (e)=> {
+            if(e.keyCode === 32){
+                brid.move(0, -100);
+            }
+        })
+})
 
+let x = Math.floor(Math.random()*4+1); 
+ctx.strokeRect( 550 , 0 ,100, [`${x}00`])
+ctx.beginPath();
+
+ctx.strokeRect( 550 , 500 ,100, [`${x}00`])
+ctx.beginPath();
 
 
 
